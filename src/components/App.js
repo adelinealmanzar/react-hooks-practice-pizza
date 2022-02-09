8465c27aa41fbdc086735df27463c511ed2f41c6
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-02-09 13:04:13 -0800

    first and second del

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+  const [pizzaFromForm, setPizzaFromForm] = useState({})
+
+  useEffect(() => {
+    fetch('http://localhost:3001/pizzas')
+    .then(r => r.json())
+    .then(fPizzas => setPizzas(fPizzas))
+  }, [])
+
+  function handlePassToForm(pizza) {
+    setPizzaFromForm(pizza)
+  }
+
   return (
     <>
       <Header />
-      <PizzaForm />
-      <PizzaList />
+      <PizzaForm pizza={pizzaFromForm}/>
+      <PizzaList pizzas={pizzas} handlePassToForm={handlePassToForm}/>
     </>
   );
 }
 
 export default App;
+
+/*
+The pizza form will then render the information about the pizza in the form, which will be editable.
+
+When the form is submitted, the information should be reflected in your table and persist in the backend.
+*/
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PizzaForm() {
+function PizzaForm({ pizza }) {
+
   return (
     <form onSubmit={null /*handle that submit*/}>
       <div className="form-row">
@@ -10,10 +11,12 @@ function PizzaForm() {
             type="text"
             name="topping"
             placeholder="Pizza Topping"
+            value={pizza.topping}
+            // onChange={(e) => setTopping(e.target.value)}
           />
         </div>
         <div className="col">
-          <select className="form-control" name="size">
+          <select className="form-control" name="size" value={pizza.size}>
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -25,7 +28,8 @@ function PizzaForm() {
               className="form-check-input"
               type="radio"
               name="vegetarian"
-              value="Vegetarian"
+              checked={pizza.vegetarian ? true : false}
+              // onChange={handleVegClick}
             />
             <label className="form-check-label">Vegetarian</label>
           </div>
@@ -34,7 +38,8 @@ function PizzaForm() {
               className="form-check-input"
               type="radio"
               name="vegetarian"
-              value="Not Vegetarian"
+              checked={pizza.vegetarian ? false : true}
+              // onClick={handleVegClick}
             />
             <label className="form-check-label">Not Vegetarian</label>
           </div>
